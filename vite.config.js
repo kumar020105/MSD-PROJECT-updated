6b00834143e1a881f2e0174ac0b4aae44156d587
kumar020105 <leelakumarchintada97@gmail.com>
2025-11-04 21:07:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+    // 👇 Allow Render’s domain (replace if your domain changes)
+    allowedHosts: ['msd-project-updated.onrender.com']
+  }
+});
